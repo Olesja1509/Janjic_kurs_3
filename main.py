1dab8bc77dffd9bcd63ba4e60ca1a078b966e13d
Olesja1509 <olesja.janjic@gmail.com>
2023-06-02 14:03:01 +0200

    add main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-from utils.utils import load_operations
+from utils.utils import formatted_operations
 
-print(load_operations())
 
+last_operations = formatted_operations(5)
+
+for i in range(len(last_operations)):
+    print(f"{last_operations[i]['date']} {last_operations[i]['description']}")
+
+    if "from" in last_operations[i]:
+        print(f"{last_operations[i]['from']} -> {last_operations[i]['to']}")
+    else:
+        print(f"{last_operations[i]['to']}")
+
+    print(f"{last_operations[i]['operationAmount']['amount']} "
+          f"{last_operations[i]['operationAmount']['currency']['name']}\n")
